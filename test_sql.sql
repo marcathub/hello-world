15dc5746d4e43d2f3d41ad9c17af20d169efded4
Marc Landvatter <marc.landvatter@gmx.de>
2016-05-01 19:50:25 +0200

    add new feature

--- test_sql.sql
+++ test_sql.sql
@@ -3,3 +3,4 @@ select 'made other changes on the master branch' from dual
 select 'fif the hotfix' from dual
 select 'did some new stuff on testing branch' from dual
 added on remote branch origin/master
+Added stuff in the neu feature_branch
